63d68fdbd08654bde54c0c486f2fa9923b1426fb
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-22 12:50:21 -0500

    Updated from Brython Server: 1/22/2019 12:50:19 PM

--- distanceFormula.py
+++ distanceFormula.py
@@ -7,4 +7,6 @@ y1=input("Enter y1:")
 x2=input("Enter x2:")
 y2=input("Enter y2:")
 
-print("The distance is:", ((y1-y2)**(y1-y2)+(x1-x2)**(x1-x2))**0.5)
+ans=((y1-y2)**(y1-y2)+(x1-x2)**(x1-x2))**0.5)
+
+print("The distance is:", ans)
