ae3cdd32d3f19bf325deab6f5686c822e4edcdc8
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-23 12:32:50 -0500

    Updated from Brython Server: 1/23/2019 12:32:50 PM

--- exponentialFunction.py
+++ exponentialFunction.py
@@ -2,4 +2,12 @@
 #1/23/18
 #exponentialFunction.py - finding a third value and creating an exponential equation
 
+x1=float(input("Enter x1:"))
+y1=float(input("Enter y1:"))
+x2=float(input("Enter x2:"))
+y2=float(input("Enter y2:"))
+x3=float(input("Enter x3:"))
 
+a=y1
+b=y2/a
+x=(x3-x1)/(x2-x1)
