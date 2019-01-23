146d0dc70320ee8c0b5341e65d543f08e6e975a4
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-23 12:54:25 -0500

    Updated from Brython Server: 1/23/2019 12:54:25 PM

--- linearSystemsAnswers.py
+++ linearSystemsAnswers.py
@@ -1,1 +1,15 @@
+#James Roth
+#1/23/18
+#linearSystemsAnswers.py - solving two linear systems for intersection
 
+a=float(input("Enter A (for Ax + By = C):"))
+b=float(input("Enter B (for Ax + By = C):"))
+c=float(input("Enter C (for Ax + By = C):"))
+d=float(input("Enter D (for Ax + By = C):"))
+e=float(input("Enter E (for Ax + By = C):"))
+f=float(input("Enter F (for Ax + By = C):"))
+
+y=(f-d*c)/(e*a-b)
+x=(c-b*y)/a
+
+print(x,y)
