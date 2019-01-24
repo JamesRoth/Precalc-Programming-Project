c194c670d4f53a3609cf8532edbe3bfe97272da8
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-24 12:48:00 -0500

    Updated from Brython Server: 1/24/2019 12:47:59 PM

--- exponentialFunction.py
+++ exponentialFunction.py
@@ -8,10 +8,12 @@ x2=float(input("Enter x2:"))
 y2=float(input("Enter y2:"))
 x3=float(input("Enter x3:"))
 
+#caclualting each value for the exponential equation
 a=y1
 b=y2/a
 x=(x3-x1)/(x2-x1)
 
+#calculating the y (answer) for the equation
 ans=a*b**x
 
 print("F (",str(x3),"):",format(ans,".3f"))
