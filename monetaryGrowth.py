a2612e98418308b7a4ec2255ef49dfd527850091
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-24 12:28:14 -0500

    Updated from Brython Server: 1/24/2019 12:28:11 PM

--- monetaryGrowth.py
+++ monetaryGrowth.py
@@ -3,15 +3,12 @@
 #monetaryGrowth.py - calculating the time it takes for money to grow to a given amount
 
 from math import log
-"""
+
 startingAmount=float(input("Enter the starting amount you want:"))
 interestRate=float(input("Enter the interest rate:"))
 compoundTimes=int(input("Enter the times compounded per year:"))
 finalAmount=float(input("Enter the final amount you want:"))
 
-ans=log(finalAmount/startingAmount)*compoundTimes/log(1+(interestRate/compoundTimes))
+ans=log((finalAmount/startingAmount),10)*compoundTimes/log((1+(interestRate/compoundTimes)),10)
 
 print("To grow from", startingAmount,"to", finalAmount, "it will take", ans,"years.")
-"""
-
-print(log(10))
