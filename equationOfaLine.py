54958159cc4b9140d6ff4c767ccd79bef86c3caf
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-25 13:08:36 -0500

    Updated from Brython Server: 1/25/2019 1:08:36 PM

--- equationOfaLine.py
+++ equationOfaLine.py
@@ -7,8 +7,12 @@ y1 = float(input("Enter y1: "))
 x2 = float(input("Enter x2: "))
 y2 = float(input("Enter y2: "))
 
-slope = format((y1-y2)/(x1-x2),".3f")
+slope = (y1-y2)/(x1-x2)
 
 b = format(y1-(slope*x1),".3f")
 
-print("The equation is Y =", slope, "*x +", b)
+slope = format(slope, ".3f")
+
+slope = str(slope) + "X"
+
+print("The equation is Y =", slope, "+", b)
