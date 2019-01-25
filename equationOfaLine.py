10674f1774c43ed3b6af9a11eac9a874a00d142a
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-25 13:03:29 -0500

    Updated from Brython Server: 1/25/2019 1:03:28 PM

--- equationOfaLine.py
+++ equationOfaLine.py
@@ -2,4 +2,9 @@
 #1/24/18
 #equationOfaLine.py - finding the equation of a line between two points
 
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
 
