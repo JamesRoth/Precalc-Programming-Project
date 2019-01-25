ef3414d7d8129474772c73ad7471b1cc8344febd
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-25 13:23:36 -0500

    Updated from Brython Server: 1/25/2019 1:23:35 PM

--- equationOfaLine.py
+++ equationOfaLine.py
@@ -15,15 +15,16 @@ if y1-y2 == 0:
 elif x1-x2 == 0:
     print("x =", x1)
 
-b = format(y1-(slope*x1),".3f")
+else:
+    b = format(y1-(slope*x1),".3f")
 
-#just formatting
-slope = format(slope, ".3f")
-slope = str(slope) + "x"
+    #just formatting
+    slope = format(slope, ".3f")
+    slope = str(slope) + "x"
 
-#more formatting, so it says - b if b is > 0 not + -b
-elif float(b) >= 0:
+    #more formatting, so it says - b if b is > 0 not + -b
+    if float(b) >= 0:
     print("The equation is Y =", slope, "+", b)
 
-elif float(b) < 0:
+    if float(b) < 0:
     print("The equation is y =", slope, b)
