53dfd7a2c25043f31f5e6198f4ac0632ec64d334
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-31 20:04:09 -0500

    Updated from Brython Server: 1/31/2019 8:04:08 PM

--- randomMulti.py
+++ randomMulti.py
@@ -5,9 +5,11 @@
 from random import randint
 correctAns = 0
 
-while correctAns < 5:
+while correctAns < 5: #loop until 5 correct answers are guessed
+    #RNG
     num1 = randint(1,10)
-    num2 = randint(1,10)
+    num2 = andint(1,10)
+    #correct answer
     ans = num1*num2
     
     question = str("What is",num1,"*",num2)
