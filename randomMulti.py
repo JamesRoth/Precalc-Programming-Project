029060ee579df1577148d1568ebd2f00315f499d
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-01-31 20:06:51 -0500

    Updated from Brython Server: 1/31/2019 8:06:50 PM

--- randomMulti.py
+++ randomMulti.py
@@ -12,7 +12,9 @@ while correctAns < 5: #loop until 5 correct answers are guessed
     #correct answer
     ans = num1*num2
     
+    #asking the user to give the answer to the multiplication problem
     guess = int(input("What is "+str(num1)+"*"+str(num2)))
+    #did they get it right?
     if guess == ans:
         correctAns+=1
     else:
